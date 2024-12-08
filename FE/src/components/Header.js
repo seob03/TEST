d69488ef112262849949074a06cf600cdfe47e55
hotte00 <junpio0812@kaist.ac.kr>
2024-12-08 19:03:29 +0900

    Header Changed, Images Loaded

--- FE/src/components/Header.js
+++ FE/src/components/Header.js
@@ -5,6 +5,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import Image from 'react-bootstrap/Image';
 
 function Header() {
     return (
@@ -12,7 +13,9 @@ function Header() {
         {['md'].map((expand) => (
           <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
             <Container fluid>
-              <Navbar.Brand href="#">MINIQUE</Navbar.Brand>
+              <Navbar.Brand href="#">
+                <Image src="./img/Logo_horizontal.svg"/>
+              </Navbar.Brand>
               <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
               <Navbar.Offcanvas
                 id={`offcanvasNavbar-expand-${expand}`}
