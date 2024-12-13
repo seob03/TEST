af9e352da7754141e8cfbe60dd903a23af325eb8
hotte00 <junpio0812@kaist.ac.kr>
2024-12-13 21:02:50 +0900

    bhbbhbb

--- FE/src/pages/Detail.js
+++ FE/src/pages/Detail.js
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from 'react';
+import Image from 'react-bootstrap/Image';
+import './Detail.css';
 
 function Detail(props) {
-    let { id } = useParams();
-    console.log(id)
+  let { id } = useParams();
+  console.log(id)
     // 페이지 결과를 상태로 관리
     const [pageResult, setPageResult] = useState([]);
 
@@ -23,16 +25,59 @@ function Detail(props) {
         
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6">
-                    디테일 페이지임
-                    <h4 className="pt-5">{pageResult.title}</h4>
-                    <p>{pageResult.content}</p>
-                    <p>{pageResult.photo && <img src={pageResult.photo} alt="Post Image" style={{ maxWidth: '100%' }} />}</p>
-                    <button className="btn btn-danger">주문하기</button>
-                </div>
+        <div>
+          <div class="container">
+            <h4 class="item">
+              디테일 페이지
+            </h4>
+          </div>
+
+          <div class="container">
+            <div class="col w600">
+              <p>{pageResult.photo && <img src={pageResult.photo} alt="Post Image" style={{ maxWidth: '100%' }} />}</p>
             </div>
+            <div class="col w380">
+              <div class="row h28">
+                {pageResult.title}
+              </div>
+              <div class="row h24">
+                가격
+              </div>
+              <div class="row h56">
+                {pageResult.content}
+              </div>
+              <div>
+                내비게이션
+              </div>
+              <div>
+                상태 설명 블록
+              </div>
+              <div>
+                판매자 버튼
+              </div>
+              <div>
+                톡하기 버튼
+              </div>
+            </div>
+          </div>
+
+          <div class="container">
+            같은 카테고리의 상품
+          </div>
+          <div class="container">
+            <div class="row eql">
+              상품 1
+            </div>
+            <div class="row eql">
+              상품 2
+            </div>
+            <div class="row eql">
+              상품 3
+            </div>
+            <div class="row eql">
+              상품 4
+            </div>
+          </div>
         </div>
     );
 }
