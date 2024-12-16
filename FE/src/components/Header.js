1483aa78a0898105c736381fb115d9cd65a742d9
hotte00 <junpio0812@kaist.ac.kr>
2024-12-17 00:06:22 +0900

    Header Style Changed

--- FE/src/components/Header.js
+++ FE/src/components/Header.js
@@ -11,35 +11,37 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 import Image from 'react-bootstrap/Image';
 
 function Header() {
-    return (
-    <header style={{width: '1024px'}}>
-      <div style={{display: 'flex', }}>
-          <div>로그아웃</div>
-          <div style={{width: '1rem'}}></div>
-          <div>내정보 </div>
+  return (
+    <header className='Header'>
+      <div className='Header-Top'>
+        <div style={{ flexGrow: 1 }}></div>
+        <div className='Header-Top-Right'>
+          <div className='Header-Top-Buttons'>로그아웃</div>
+          <div className='Header-Top-Buttons'>내정보 </div>
+        </div>
       </div>
       <div className='Header-First'>
-        <div className = 'Header-First-Logo'>
-          <Navbar.Brand href="#">
-            <Image src="./img/Logo_horizontal.svg"/>
-          </Navbar.Brand>
+        <div className='Header-First-Logo'>
+          <a href="#">
+            <Image src="./img/Logo_horizontal.svg" />
+          </a>
         </div>
-        <div className= 'Header-First-SearchBar'>
-          <SearchBar/>
+        <div className='Header-First-SearchBar'>
+          <SearchBar />
         </div>
         <div className='Header-First-Menu'>
-          <Nav.Link href="#action1">채팅내역</Nav.Link>
-          <Nav.Link href="#action2">판매내역</Nav.Link>
-          <Nav.Link href="#action3">내 상점</Nav.Link>
+          <div href="#action1" className='Header-First-Menu-Buttons'>채팅내역</div>
+          <div href="#action2" className='Header-First-Menu-Buttons'>판매내역</div>
+          <div href="#action3" className='Header-First-Menu-Buttons'>내 상점</div>
         </div>
       </div>
-      <div style={{margin: '10px', display: 'flex'}}>
-          <div>BEST</div>
-          <div>GIRLS</div>
-          <div>BOYS</div>
+      <div style={{ display: 'flex', margintop: '20px' }}>
+        <div>BEST</div>
+        <div>GIRLS</div>
+        <div>BOYS</div>
       </div>
     </header>
-    );
+  );
 }
-  
+
 export default Header;
--- FE/src/pages/Detail.js
+++ FE/src/pages/Detail.js
@@ -2,86 +2,84 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import Image from 'react-bootstrap/Image';
 import './Detail.css';
-import Image from 'react-bootstrap/Image';
-import './Detail.css';
 
 function Detail(props) {
   let { id } = useParams();
   console.log(id)
-    // 페이지 결과를 상태로 관리
-    const [pageResult, setPageResult] = useState([]);
+  // 페이지 결과를 상태로 관리
+  const [pageResult, setPageResult] = useState([]);
 
-    useEffect(() => {
-        // fetch 요청
-        fetch('/detail/' + id)
-        .then((response) => response.json())
-        .then((result) => {
-            // 상태 업데이트
-            setPageResult(result);
-            console.log(result); // 데이터를 콘솔에 출력
-        })
-        .catch((error) => {
-            console.error('Error fetching data:', error);
-        });
-    }, [id]); // id가 바뀔 때마다 요청을 다시 보냄
-        
+  useEffect(() => {
+    // fetch 요청
+    fetch('/detail/' + id)
+      .then((response) => response.json())
+      .then((result) => {
+        // 상태 업데이트
+        setPageResult(result);
+        console.log(result); // 데이터를 콘솔에 출력
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, [id]); // id가 바뀔 때마다 요청을 다시 보냄
 
-    return (
-        <div>
-          <div class="container">
-            <h4 class="item">
-              디테일 페이지
-            </h4>
+
+  return (
+    <div>
+      <div class="container">
+        <h4 class="item">
+          디테일 페이지
+        </h4>
+      </div>
+
+      <div class="container">
+        <div class="col w600">
+          <img src={pageResult.photo} />
+        </div>
+        <div class="col w380">
+          <div class="row h28">
+            {pageResult.title}
           </div>
-
-          <div class="container">
-            <div class="col w600">
-              <img src={pageResult.photo}/>
-            </div>
-            <div class="col w380">
-              <div class="row h28">
-                {pageResult.title}
-              </div>
-              <div class="row h24">
-                가격
-              </div>
-              <div class="row h56">
-                {pageResult.content}
-              </div>
-              <div>
-                내비게이션
-              </div>
-              <div>
-                상태 설명 블록
-              </div>
-              <div>
-                판매자 버튼
-              </div>
-              <div>
-                톡하기 버튼
-              </div>
-            </div>
+          <div class="row h24">
+            가격
           </div>
-
-          <div class="container">
-            같은 카테고리의 상품
+          <div class="row h56">
+            {pageResult.content}
           </div>
-          <div class="container">
-            <div class="row eql">
-              상품 1
-            </div>
-            <div class="row eql">
-              상품 2
-            </div>
-            <div class="row eql">
-              상품 3
-            </div>
-            <div class="row eql">
-              상품 4
-            </div>
+          <div>
+            내비게이션
+          </div>
+          <div>
+            상태 설명 블록
+          </div>
+          <div>
+            판매자 버튼
+          </div>
+          <div>
+            톡하기 버튼
           </div>
         </div>
-    );
+      </div>
+
+      <div class="container">
+        같은 카테고리의 상품
+      </div>
+      <div class="container">
+        <div class="row eql">
+          상품 1
+        </div>
+        <div class="row eql">
+          상품 2
+        </div>
+        <div class="row eql">
+          상품 3
+        </div>
+        <div class="row eql">
+          상품 4
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
